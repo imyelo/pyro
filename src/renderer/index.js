4520af5f6a28a89545682fdd090d9d4ecfb237ea
imyelo <zhihuzeye@gmail.com>
2018-04-05 02:48:13 +0800

    fix: axios always uses Node.js http adapter

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,10 +2,14 @@ import 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 import store from './store'
 import router from './router'
 import './index.css'
 import App from './app.vue'
+
+// always uses Node.js http adapter in axios. See https://github.com/axios/axios/issues/552
+axios.defaults.adapter = require('axios/lib/adapters/http')
 
 new Vue({
   el: '#app',
