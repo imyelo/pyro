88aea5b779c5e6b5288bd475d2339bd377f7d9a6
imyelo <zhihuzeye@gmail.com>
2018-04-05 04:16:58 +0800

    feat: add imported data preview

--- src/renderer/views/import.vue
+++ src/renderer/views/import.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="main">
     <Navigator :backable="!isSetup" />
-    <div class="view" v-bind:class="{ dragging: isDragging }"
-      v-on:dragover.prevent.stop="isDragging = true" v-on:dragleave="isDragging = false"
-      v-on:drop.prevent.stop="drop" v-on:click="select">
-      <p><img src="../assets/floppy.png" draggable="false" /></p>
-      <p>Drop the data file here.</p>
-      <input type="file" v-on:change="_select" hidden ref="file" />
-    </div>
+    <transition name="transition-fade" mode="out-in">
+      <div v-if="!devices.length" key="import" class="view view-import" v-bind:class="{ dragging: isDragging }"
+        v-on:dragover.prevent.stop="isDragging = true" v-on:dragleave="isDragging = false"
+        v-on:drop.prevent.stop="drop" v-on:click="select">
+        <p><img src="../assets/floppy.png" draggable="false" /></p>
+        <p>Drop the data file here.</p>
+        <input type="file" v-on:change="_select" hidden ref="file" />
+      </div>
+      <div v-else key="preview" class="view view-preview" v-bind:class="{ dragging: isDragging }"
+        v-on:dragover.prevent.stop="isDragging = true" v-on:dragleave="isDragging = false"
+        v-on:drop.prevent.stop="drop">
+        <div class="sheet">
+          <table>
+            <thead>
+              <tr>
+                <th v-for="key in keys" :key="key">
+                  {{ key }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(device, index) in devices" :key="index">
+                <td v-for="key in keys" :key="key">
+                  {{ device[key] }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="actions">
+          <button class="button reset" v-on:click="reset">Reset</button>
+          <button class="button" v-on:click="save">Save</button>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -23,15 +51,22 @@ export default {
   data () {
     return {
       isDragging: false,
+      devices: this.$store.state.devices,
     }
   },
   computed: {
     isSetup () {
       return 'setup' in this.$route.query
     },
+    keys () {
+      if (!this.devices[0]) {
+        return []
+      }
+      return Object.keys(this.devices[0])
+    },
   },
   mounted () {
-    remote.getCurrentWindow().setSize(400, 400, true)
+    remote.getCurrentWindow().setSize(600, 600, true)
   },
   methods: {
     select () {
@@ -55,12 +90,18 @@ export default {
       let devices = await csv.parseAsync(plain, {
         columns: true,
       })
-      this.$store.dispatch('setDevices', devices)
+      this.devices = devices
+    },
+    reset () {
+      this.devices = []
+    },
+    save () {
+      this.$store.dispatch('setDevices', this.devices)
       if (this.isSetup) {
         return this.$router.push('/ports?setup')
       }
       this.$router.back()
-    },
+    }
   },
   components: {
     Navigator,
@@ -69,7 +110,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.view {
+.view.view-import {
   -webkit-app-region: no-drag;
   display: flex;
   justify-content: center;
@@ -93,5 +134,136 @@ export default {
       opacity: 0.5;
     }
   }
+}
+
+.view.view-preview {
+  color: #333;
+  font-size: 12px;
+  padding: 0 48px 48px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  &.dragging {
+    opacity: 0.5;
+  }
+
+  .button {
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 0 1em;
+    box-sizing: border-box;
+    text-align: center;
+    font-size: 24px;
+    height: 3em;
+    border: none;
+    outline: none;
+    -webkit-app-region: no-drag;
+    background-color: hsl(204, 100%, 50%);
+    box-shadow: 0 8px hsl(204, 100%, 20%);
+    color: #fff;
+    cursor: pointer;
+    transition: all 200ms ease;
+    &:hover {
+      background-color: hsl(204, 100%, 70%);
+    }
+    &:active {
+      background-color: hsl(204, 100%, 40%);
+    }
+    &:disabled {
+      background-color: rgba(0,0,0,0.25);
+      color: rgba(255,255,255,0.5);
+      box-shadow: none;
+    }
+  }
+  .actions {
+    display: flex;
+    .reset {
+      margin-right: 24px;
+      background-color: hsl(0, 0%, 60%);
+      box-shadow: 0 8px hsl(0, 0%, 30%);
+      &:hover {
+        background-color: hsl(0, 0%, 80%);
+      }
+      &:active {
+        background-color: hsl(0, 0%, 50%);
+      }
+    }
+  }
+
+  .sheet {
+    flex: 1;
+    margin-bottom: 24px;
+    overflow: hidden;
+    table {
+      height: 100%;
+      width: 100%;
+      font-family: "Ubuntu Mono";
+      line-height: 2em;
+      margin-bottom: 24px;
+      border-collapse: collapse;
+      display: flex;
+      flex-flow: column;
+      border: 2px solid #ddd;
+      border-right: none;
+      box-sizing: border-box;
+      th, td {
+        padding: 1.5em 2em;
+        line-height: 1em;
+        text-align: left;
+        word-wrap: break-word;
+        word-break: break-all;
+      }
+      thead {
+        flex: 0 0 auto;
+        width: 100%;
+        display: table;
+        table-layout: fixed;
+        tr {
+          background-color: #ddd;
+        }
+      }
+      tbody {
+        flex: 1 1 auto;
+        display: block;
+        overflow-y: scroll;
+        background-color: #ddd;
+        -webkit-app-region: no-drag;
+        tr {
+          width: 100%;
+          display: table;
+          table-layout: fixed;
+          background-color: #eee;
+          transition: all 200ms ease;
+          -webkit-user-select: text;
+          &:nth-child(2n) {
+            background-color: #e6e6e6;
+          }
+          &:hover {
+            background-color: #f6f6f6;
+          }
+        }
+
+        &::-webkit-scrollbar-track {
+          background-color: transparent;
+        }
+        &::-webkit-scrollbar {
+          width: 2px;
+          background-color: #ddd;
+        }
+        &::-webkit-scrollbar-thumb {
+          background-color: #666;
+        }
+      }
+    }
+  }
+}
+
+.transition-fade-enter-active, .transition-fade-leave-active {
+  transition: all ease .4s;
+}
+.transition-fade-enter, .transition-fade-leave-to {
+  opacity: 0;
+  transform: translateY(-12px);
 }
 </style>
